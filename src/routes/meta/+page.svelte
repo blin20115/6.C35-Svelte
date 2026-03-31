39f71d30faa2bc2b11be103656de031b9d0fb8f8
blin20115 <blin20115@gmail.com>
2026-03-31 13:55:50 -0400

    Visualizing time and day of commits in a scatterplot

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -4,8 +4,23 @@
     import * as d3 from "d3";
     import BarHorizontal from "$lib/BarHorizontal.svelte";
 
+    let width = 1000,
+        height = 600;
+    let margin = { top: 10, right: 10, bottom: 30, left: 20 };
+    let usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+    };
+    usableArea.width = usableArea.right - usableArea.left;
+    usableArea.height = usableArea.bottom - usableArea.top;
+
     let locData = [];
+    let commits = [];
     let barData = [];
+
+    let xAxis, yAxis, yAxisGridlines;
 
     onMount(async () => {
         locData = await d3.csv(`${base}/loc.csv`, (row) => ({
@@ -13,7 +28,29 @@
             line: Number(row.line),
             length: Number(row.length),
             depth: Number(row.depth),
+            date: new Date(row.date + "T00:00" + row.timezone),
+            datetime: new Date(row.datetime),
         }));
+
+        commits = d3
+            .groups(locData, (d) => d.commit)
+            .map(([commit, lines]) => {
+                let first = lines[0];
+                let { author, date, time, timezone, datetime } = first;
+                return {
+                    id: commit,
+                    url:
+                        "https://github.com/vis-society/lab-7/commit/" + commit,
+                    author,
+                    date,
+                    time,
+                    timezone,
+                    datetime,
+                    hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+                    totalLines: lines.length,
+                    lines: lines,
+                };
+            });
 
         barData = d3
             .rollups(
@@ -24,6 +61,33 @@
             .sort((a, b) => b[1] - a[1])
             .map(([language, lines]) => ({ label: language, value: lines }));
     });
+
+    $: [minDate, maxDate] = d3.extent(commits.map((d) => d.date));
+    $: maxDatePlusOne = new Date(maxDate);
+    $: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+
+    $: xScale = d3
+        .scaleTime()
+        .domain([minDate, maxDatePlusOne])
+        .range([usableArea.left, usableArea.right])
+        .nice();
+
+    $: yScale = d3
+        .scaleLinear()
+        .domain([24, 0])
+        .range([usableArea.bottom, usableArea.top]);
+
+    $: {
+        d3.select(xAxis).call(d3.axisBottom(xScale));
+        d3.select(yAxis).call(
+            d3
+                .axisLeft(yScale)
+                .tickFormat((d) => String(d % 24).padStart(2, "0") + ":00"),
+        );
+        d3.select(yAxisGridlines).call(
+            d3.axisLeft(yScale).tickFormat("").tickSize(-usableArea.width),
+        );
+    }
 </script>
 
 <svelte:head>
@@ -33,4 +97,35 @@
 <h1>Meta</h1>
 <p>Stats about the codebase behind this site.</p>
 
+<h3>Commits by time of day</h3>
+<svg viewBox="0 0 {width} {height}">
+    <g
+        class="gridlines"
+        transform="translate({usableArea.left}, 0)"
+        bind:this={yAxisGridlines}
+    />
+    <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+    <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+    <g class="dots">
+        {#each commits as commit, index}
+            <circle
+                cx={xScale(commit.datetime)}
+                cy={yScale(commit.hourFrac)}
+                r="5"
+                fill="steelblue"
+            />
+        {/each}
+    </g>
+</svg>
+
 <BarHorizontal data={barData} />
+
+<style>
+    svg {
+        overflow: visible;
+    }
+
+    .gridlines {
+        stroke-opacity: 0.2;
+    }
+</style>
